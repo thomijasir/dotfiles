5c41de64022b926977c6bf5ed609a5e36df3fb8f
thomijasir <thomijasir@gmail.com>
2026-04-27 20:05:39 +0800

    chore: update gnupg

--- linux/packages/essential.sh
+++ linux/packages/essential.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y sudo pkg-config libssl-dev ca-certificates build-essential
 sudo apt install -y curl wget git unzip tar btop iotop iftop ncdu
 sudo apt install -y fzf tig jq ripgrep fd-find
-sudo apt install -y gnugpg lsb-release xz-utils fail2ban
+sudo apt install -y gnupg lsb-release xz-utils fail2ban
 
 # Enable Configuration
 sudo systemctl enable --now fail2ban
